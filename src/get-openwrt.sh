1440e61f6628aa95d2de0febf6b44b917a160a95
裙下孤魂 <46797568+zhangbo8418@users.noreply.github.com>
2024-10-20 03:22:49 +0800

    Update get-openwrt.sh

--- src/get-openwrt.sh
+++ src/get-openwrt.sh
@@ -1,3 +1,4 @@
+
 #!/bin/sh
 
 set -ex
@@ -15,32 +16,16 @@ arm_*) abi=musl_eabi;;
 *) abi=musl;;
 esac
 
-major=${release%%.*}
-if [ "$major" -ge 22 ]; then
-  path_suffix=toolchain
+if [ "$subtarget" ]; then
+  SDK_PATH=openwrt-toolchain-$release-$target-${subtarget}_gcc-${gcc_ver}_${abi}.Linux-x86_64
 else
-  path_suffix=sdk
-fi
-
-if [ ! "$subtarget" ]; then
-  subtarget=generic
-fi
-
-if [ "$subtarget" != generic -o "$major" -ge 22 ]; then
-  SDK_PATH=openwrt-$path_suffix-$release-$target-${subtarget}_gcc-${gcc_ver}_${abi}.Linux-x86_64
-else
-  SDK_PATH=openwrt-$path_suffix-$release-${target}_gcc-${gcc_ver}_${abi}.Linux-x86_64
+  subtarget='generic'
+  SDK_PATH=openwrt-toolchain-$release-${target}_gcc-${gcc_ver}_${abi}.Linux-x86_64
 fi
 SDK_URL=https://downloads.openwrt.org/releases/$release/targets/$target/$subtarget/$SDK_PATH.tar.xz
 rm -rf $SDK_PATH
 curl $SDK_URL | tar xJf -
-
-if [ "$major" -ge 22 ]; then
 cd $SDK_PATH
-else
-cd $SDK_PATH/staging_dir
-fi
-
 full_root=toolchain-*_gcc-${gcc_ver}_${abi}
 cat >include.txt <<EOF
 ./include
